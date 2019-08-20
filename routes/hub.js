0ff2bc6af8b6cfe2f42e60c74bc75d96fcd8614f
nguyentamdat <nguyentamdat@gmail.com>
2019-08-20 16:36:08 +0700

    add fail code

--- routes/hub.js
+++ routes/hub.js
@@ -1,18 +1,31 @@
 var express = require("express");
 var router = express.Router();
 var crypto = require("crypto");
+var jwt = require("jsonwebtoken");
+var passportJWT = require("passport-jwt");
+var ExtractJwt = passportJWT.ExtractJwt;
+
+var jwtOptions = {};
+jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+jwtOptions.secretOrKey = "wowwow";
 
 /* GET users listing. */
 router.post("/login", (req, res, next) => {
-  console.log(req.body.hubMac);
-  var token = crypto.randomBytes(48).toString("hex");
-  console.log(`Token: ${token}`);
-  res.send({
-    retCode: 1000,
-    retData: {
-      token: token
-    }
-  });
+  try {
+    console.log(req.body.hubMac);
+    const { hubMac } = req.body;
+    var payload = { id: hubMac };
+    var token = jwt.sign(payload, jwtOptions.secretOrKey);
+    console.log(`Token: ${token}`);
+    res.json({
+      retCode: 1000,
+      retData: {
+        token: token
+      }
+    });
+  } catch {
+    res.json({ retCode: 1002 });
+  }
 });
 
 router.post("/location", (req, res, next) => {
@@ -20,7 +33,7 @@ router.post("/location", (req, res, next) => {
   const tags = req.body.tags;
   const token = req.headers["authorization"];
   console.log(`Token: ${token}`);
-  res.send({ retCode: 1000 });
+  res.json({ retCode: 1000 });
 });
 
 module.exports = router;
